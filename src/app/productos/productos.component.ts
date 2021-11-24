3d29185be3b832c4a02ad5e7eb55a61277250d06
JorgePinillaValera <pinillavalera@gmail.com>
2021-11-24 16:16:40 +0100

    creación de interface+ css + ngFor

--- src/app/productos/productos.component.ts
+++ src/app/productos/productos.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Producto } from '../interfaces/producto.interface';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.css']
+  styleUrls: ['./productos.component.css'],
 })
 export class ProductosComponent implements OnInit {
+  @Input() productos: Producto[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.productos = [];
   }
 
+  ngOnInit(): void {}
 }
