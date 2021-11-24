76f39b9c9b562e4906db6862059efa1baacc24df
Ferrp86 <freviriegop86@gmail.com>
2021-11-24 17:50:15 +0100

    Agregada ruta series

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { SeriesComponent } from './series/series.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent },
-  { path: 'usuarios', component: UsuariosComponent }
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'series', component: SeriesComponent }
 ]
 
 @NgModule({
